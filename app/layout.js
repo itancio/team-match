970b6833f7002a607d65427d73a6c6ebca73b60b
Irvin Tancioco <irvin.tanc@gmail.com>
2024-08-23 14:44:00 -0700

    clean slate

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,5 @@
-
 import { Inter } from "next/font/google";
-import Header from "@/components/Header";
 import "./globals.css";
-
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,13 +9,9 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
-  
   return (
-      <html lang="en">
-        <body className={inter.className}>
-              <Header />
-              {children}
-        </body>        
-      </html>
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
   );
 }
